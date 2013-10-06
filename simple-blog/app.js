b94ed9b90e3dedc1c82c7e85acf8ab578b19cfe2
julian3833 <julianpeller@gmail.com>
2013-10-06 02:58:31 -0300

    article provider

--- simple-blog/app.js
+++ simple-blog/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var ArticleProvider = require('./article-provider-memory').ArticleProvider;
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -27,9 +29,59 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
+
+app.get('/default-index', routes.index);
+
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+/*Blog old code*/
+/*
+
+var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views'); //equivalent
+  app.set('view engine', 'jade'); //equivalent
+  app.use(express.bodyParser()); //equivalent
+  app.use(express.methodOverride()); //equivalent
+  app.use(require('stylus').middleware({ src: __dirname + '/public' })); //
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
+
+app.listen(3000);
+
+*/
